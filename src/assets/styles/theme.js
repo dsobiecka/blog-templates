4cf215c12daa8f1bb42f2b98ff281f43a586fb50
Dominika Sobiecka <dominika.sobiecka@gmail.com>
2022-03-24 12:50:06 +0100

    adding global style and color themes, fonts

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    lightGrey: '#F7F8FA',
+    grey: '#C0C7D6',
+    darkGrey: '#737C8E',
+    black: '#101010',
+    green: '#80D1B0',
+  },
+  fontSize: {
+    ld: '60px',
+    md: '27px',
+    sd: '15px',
+    lm: '54px',
+    mm: '24px',
+    sm: '16px',
+  },
+};
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,5 +1,16 @@
+import { theme } from '../assets/styles/theme';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../assets/styles/GlobalStyle';
+
 function Root() {
-  return <div className="App">test</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        test
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default Root;
