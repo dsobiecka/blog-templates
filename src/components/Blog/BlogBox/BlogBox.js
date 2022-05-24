7764d6a201224b354363c78dd0b979042e4e7f32
Dominika Sobiecka <dominika.sobiecka@gmail.com>
2022-05-24 23:50:01 +0200

    added styles in blog

--- src/components/Blog/BlogBox/BlogBox.js
+++ src/components/Blog/BlogBox/BlogBox.js
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types';
 import { Wrapper, DateAdded, ShortDescription } from './BlogBox.styles';
+import { Route } from 'react-router-dom';
+import Blog from '../Blog';
+import { articles } from '../../../data/articles';
 
 const BlogBox = (props) => {
   return (
-    <Wrapper className={`box box${props.id}`}>
-      <DateAdded>{props.data.toISOString()}</DateAdded>
-      <ShortDescription>{props.shortDescription}</ShortDescription>
-    </Wrapper>
+    <Route path='/blog/:id'>
+      <Wrapper className={`box box${props.id}`}>
+        <DateAdded>{props.data.toISOString()}</DateAdded>
+        <ShortDescription>{props.shortDescription}</ShortDescription>
+      </Wrapper>
+    </Route>
   );
 };
 
@@ -14,7 +19,7 @@ BlogBox.propTypes = {
   data: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  shortDescription: PropTypes.string,
+  shortDescription: PropTypes.string
 };
 
 export default BlogBox;
