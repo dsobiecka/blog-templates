974384de3b153ed8bf2468ebece92b0de6b522c9
Dominika Sobiecka <dominika.sobiecka@gmail.com>
2022-05-26 22:38:18 +0200

    changed date format

--- src/components/Main/Article/Article.js
+++ src/components/Main/Article/Article.js
@@ -2,10 +2,16 @@ import PropTypes from 'prop-types';
 import { Wrapper, WhiteBox, DateAdded, ShortDescription, BoxTitle } from './Article.styles';
 
 const Article = (props) => {
+  const month = props.data.toLocaleString('en-US', { month: 'long' });
+  const year = props.data.getFullYear();
+  const day = props.data.toLocaleString('en-US', { day: '2-digit' });
+
   return (
     <Wrapper className={`box box${props.id}`} style={{ backgroundImage: `url(${props.img})` }}>
       <WhiteBox>
-        <DateAdded>{props.data.toISOString()}</DateAdded>
+        <DateAdded>
+          <div>{day}/{month}/{year}</div>
+        </DateAdded>
         <BoxTitle>{props.title}</BoxTitle>
         <ShortDescription>{props.shortDescription}</ShortDescription>
       </WhiteBox>
@@ -17,7 +23,7 @@ Article.propTypes = {
   data: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  shortDescription: PropTypes.string,
+  shortDescription: PropTypes.string
 };
 
 export default Article;
