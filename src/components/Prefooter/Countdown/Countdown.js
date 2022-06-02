21d72b3d626c25a3dd4a3659f83f13e08bf8496d
Dominika Sobiecka <dominika.sobiecka@gmail.com>
2022-06-02 14:30:11 +0200

    adding counter

---
+++ src/components/Prefooter/Countdown/Countdown.js
@@ -0,0 +1,9 @@
+import dayjs from 'dayjs';
+
+export function getRemainingTime(timestamMs) {
+  return {
+    days: '31',
+    hours: '12',
+    minutes: '30',
+  };
+}
--- src/components/Prefooter/PrefooterLogin/PrefooterLogin.js
+++ src/components/Prefooter/PrefooterLogin/PrefooterLogin.js
@@ -1,9 +1,36 @@
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getRemainingTime } from '../Countdown/Countdown';
 
-const Prefooterlogin = (props) => {
+const defaultRemainingTime = {
+  days: '00',
+  hours: '00',
+  minutes: '00',
+};
+
+const Prefooterlogin = (props, {countdownTimestampMs}) => {
+  const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      updateRemainingTime(countdownTimestampMs);
+    }, 1000);
+    return () => clearInterval(intervalID);
+  }, [countdownTimestampMs]);
+
+  function updateRemainingTime(countdown) {
+    getRemainingTime(countdown);
+  }
+
   return (
     <ul className="list-inner">
+      <div>
+        <span>{`${remainingTime.days} days / `}</span>
+        <span>{`${remainingTime.hours} hours / `}</span>
+        <span>{`${remainingTime.minutes} minutes`}</span>
+      </div>
+
       {props.onLogout && (
         <li className="nav-item">
           <NavLink to={`/login`}>Login</NavLink>
@@ -18,7 +45,5 @@ const Prefooterlogin = (props) => {
     </ul>
   );
 };
-
-Prefooterlogin.propTypes = {};
 
 export default Prefooterlogin;
